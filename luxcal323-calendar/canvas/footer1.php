4ecf8143c6d34f394fe7f05b5f646553f6ab7e40
Automata-Development <cole@teamautomata.com>
2016-03-24 00:22:28 -0700

    LOD Website 3/23 Files
    Added files referenced by the main index file

---
+++ luxcal323-calendar/canvas/footer1.php
@@ -0,0 +1,16 @@
+<?php
+/*
+= Footer for LuxCal event calendar pages - Normal =
+
+© Copyright 2009-2014 LuxSoft - www.LuxSoft.eu
+*/
+
+?>
+</div>
+<footer class="noPrint">
+<?php
+echo "<span class='floatR'><a href='http://www.luxsoft.eu' target='_blank'><span class='footLB'>Lux</span><span class='footLR'>Soft</span></a></span>\n";
+?>
+</footer>
+</body>
+</html>
---
+++ luxcal323-calendar/lcconfig.php
@@ -0,0 +1,15 @@
+<?php
+/*
+= LuxCal event calendar configuration =
+
+© Copyright 2009-2014 LuxSoft - www.LuxSoft.eu
+
+This file is part of the LuxCal Web Calendar.
+*/
+$lcc="3.2.3"; //current LuxCal version
+$dbHost="mysql.lightofthedesert.dreamhosters.com"; //MySQL server
+$dbUnam="lod_admin"; //database username
+$dbPwrd="matt.5.14"; //database password
+$dbName="lightofthedesertcalendar"; //database name
+$dbPfix="mycal"; //db table name prefix (default calendar)
+?>

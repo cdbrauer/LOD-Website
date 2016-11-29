67d55752060f02c3a73ff5058449b9a530bf06a3
Automata-Development <judebrauer@Judes-MacBook-Air.local>
2016-11-28 18:52:07 -0700

    Work towards database integration
    Firebase is now set up and working. The “getChurchEvent” JS function
    can be used to retrieve the HTML for the specified event. Loading a
    list of events is not working currently.

--- js/database.js
+++ js/database.js
@@ -1,5 +1,55 @@
-function testing() {
-    firebase.database().ref('/church/adult/0').once('value').then(function(snapshot) {
-        document.write(snapshot.val().d1);
+function getEventsForGroup(divId, group) {
+    var i = 0;
+
+    while(true) {
+        var success = getChurchEventBlocking(divId, group, i);
+
+        if(success == false) {
+            break;
+        }
+
+        i++;
+    }
+}
+
+function getChurchEventBlocking(divId, group, index) {
+    var success = true;
+    var done = false;
+
+    firebase.database().ref('church/' + group + '/' + index).once('value').then(function(snapshot) {
+        if(snapshot.exists()) {
+            var event = snapshot.val();
+            var html = '<h2>' + event.title + '</h2>';
+            html += '<p>' + event.age + '</p>';
+            html += '<p>' + event.time + '</p>';
+            html += '<p>' + event.room + '</p>';
+            html += '<p>' + event.contact + '</p>';
+
+            document.getElementById(divId).innerHTML = html;
+
+            success = true;
+        }
+        else {
+            success = false;
+        }
+
+        done = true;
+    });
+
+    while(done == false);
+
+    return success;
+}
+
+function getChurchEvent(divId, group, index) {
+    firebase.database().ref('church/' + group + '/' + index).once('value').then(function(snapshot) {
+        var event = snapshot.val();
+        var html = '<h2>' + event.title + '</h2>';
+        html += '<p>' + event.age + '</p>';
+        html += '<p>' + event.time + '</p>';
+        html += '<p>' + event.room + '</p>';
+        html += '<p>' + event.contact + '</p>';
+
+        document.getElementById(divId).innerHTML = html;
     });
 }

ac7d720a1080dd39d1c1497e953990a82ff2ff4d
Automata-Development <jude@teamautomata.com>
2016-12-26 18:19:44 -0700

    Work on database integration
    Database API changes, work on integrating community page with database.

--- js/database.js
+++ js/database.js
@@ -5,13 +5,13 @@
  * convert the JSON object into formatted text using the given function,
  * and insert the data into HTML inside the specified div.
  */
-function getDataList(path, dataAdder, divId) {
-    firebase.database().ref(path).once('value').then(function(snapshot) {
+function getDataList(path, dataConverter, divId) {
+    firebase.database().ref(path).once("value").then(function(snapshot) {
         var events = snapshot.val();
 
         for (var index in events) {
             if (events.hasOwnProperty(index)) {
-                dataAdder(divId, events[index]);
+                addTextToDiv(divId, dataConverter(events[index]));
             }
         }
     });
@@ -22,27 +22,12 @@ function getDataList(path, dataAdder, divId) {
  *
  * This will retrieve the data from the given path in the database,
  * convert the JSON object into formatted text using the given function,
- * and insert the data into HTML inside the provided divId.
+ * and insert the data into HTML inside the specified div.
  */
-function getData(path, dataAdder, divId) {
-    firebase.database().ref(path).once('value').then(function(snapshot) {
-        dataAdder(divId, snapshot.val());
+function getData(path, dataConverter, divId) {
+    firebase.database().ref(path).once("value").then(function(snapshot) {
+        addTextToDiv(divId, dataConverter(snapshot.val()));
     });
-}
-
-/**
- * Function that converts the JSON data representing a church event into formatted HTML
- */
-function addEvent(divId, event) {
-    var html = '';
-
-    html = '<h2>' + event.title + '</h2>';
-    html += '<p>' + event.age + '</p>';
-    html += '<p>' + event.time + '</p>';
-    html += '<p>' + event.room + '</p>';
-    html += '<p>' + event.contact + '</p>';
-
-    addTextToDiv(html);
 }
 
 /**
@@ -50,4 +35,49 @@ function addEvent(divId, event) {
  */
 function addTextToDiv(divId, text) {
     document.getElementById(divId).innerHTML += text;
+}
+
+function addElement(elementType, content) {
+    if (content != null && content != "" && content != "Null") {
+        var emailRegex = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
+        content = content.replace(emailRegex, '<a href="mailto:$1">$1</a>');
+
+        return "<" + elementType + ">" + content + "</" + elementType + ">";
+    }
+    else {
+        return "";
+    }
+}
+
+/**
+ * Collection of functions for converting different types of JSON data from Firebase into HTML
+ */
+var DataConverters = {
+    /**
+     * Function that converts the JSON data representing a church event into formatted HTML
+     */
+    event: function(event) {
+        var html = "";
+
+        html += addElement("h2", event.title); //'<h2>' + event.title + '</h2>';
+        html += addElement("p", event.subtitle);
+        html += addElement("p", event.age);
+        html += addElement("p", event.time);
+        html += addElement("p", event.room);
+        html += addElement("p", event.d1);
+        html += addElement("p", event.d2);
+        html += addElement("p", event.contact);
+
+        return html;
+    },
+
+    ministry: function(ministry) {
+        var html = "";
+
+        html += addElement("h2", ministry.title);
+        html += addElement("p", ministry.d);
+        html += "<a href=\"http://" + ministry.link + "\">" + ministry.link + "</a>";
+
+        return html;
+    }
 }

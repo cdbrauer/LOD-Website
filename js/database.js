0736295ca911540e6d1e876da6e9f97d7af3cb82
Automata-Development <jude@teamautomata.com>
2016-12-27 18:26:46 -0700

    Work on database integration
    The three What We Do pages and the Links page are all fully integrated
    with the database. The service times on the Services page are also
    reading from the database.

--- js/database.js
+++ js/database.js
@@ -37,6 +37,9 @@ function addTextToDiv(divId, text) {
     document.getElementById(divId).innerHTML += text;
 }
 
+/**
+ * Helper function for converting text into HTML elements
+ */
 function addElement(elementType, content) {
     if (content != null && content != "" && content != "Null") {
         var emailRegex = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
@@ -54,7 +57,7 @@ function addElement(elementType, content) {
  */
 var DataConverters = {
     /**
-     * Function that converts the JSON data representing a church event into formatted HTML
+     * Function that converts the JSON data representing a event on the In Our Church page into formatted HTML
      */
     event: function(event) {
         var html = "";
@@ -71,12 +74,82 @@ var DataConverters = {
         return html;
     },
 
+    /**
+     * Function that converts the JSON data representing a ministry on the Community page into formatted HTML
+     */
     ministry: function(ministry) {
         var html = "";
 
         html += addElement("h2", ministry.title);
         html += addElement("p", ministry.d);
-        html += "<a href=\"http://" + ministry.link + "\">" + ministry.link + "</a>";
+        html += addElement("p", ministry.d1);
+
+        var link = ministry.link;
+
+        if (link != null && link != "" && link != "Null") {
+            html += `<p>For more information, see <a href="http://${link}" target="_blank">${link}</a></p>`;
+        }
+
+        return html;
+    },
+
+    /**
+     * Function that converts the JSON data representing a website on the Links page into formatted HTML
+     */
+    website: function(site) {
+        var html = "";
+
+        html += addElement("h2", site.title);
+
+        var link = site.link;
+
+        if (link != null && link != "" && link != "Null") {
+            html += `<p><a href="http://${link}" target="_blank">${link}</a></p>`;
+        }
+
+        html += addElement("p", site.d);
+
+        return html;
+    },
+
+    /**
+     * Function that converts the JSON data representing an app on the Links page into formatted HTML
+     */
+    app: function(app) {
+        var html = "";
+
+        html += '<div class="appIcons">';
+        html += `<a href="${app.gpLink}" target="_blank"><img alt="Googly Play" src="img/google-play.png"/></a>`;
+        html += `<a href="${app.asLink}" target="_blank"><img alt="Apple App Store" src="img/apps-store.png"/></a>`;
+        html += '</div>';
+        html += addElement("h2", app.title);
+        html += addElement("p", app.d);
+
+        var link = app.onlineVersion;
+
+        if (link != null && link != "" && link != "Null") {
+            html += `<p>Check out the online version at <a href="http://${link}" target="_blank">${link}</a></p>`;
+        }
+
+        html += '<div class="blankline" style="height: 0;">&nbsp;</div>';
+
+        return html;
+    },
+
+    /**
+     * Function that converts a string into a HTML list item
+     */
+    listItem: function(item) {
+        var html = addElement("li", item);
+
+        return html;
+    },
+
+    /**
+     * Function that converts a string into HTML bold text
+     */
+    boldText: function(text) {
+        var html = addElement("b", text);
 
         return html;
     }

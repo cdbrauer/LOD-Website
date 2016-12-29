4a1682d1df33d966455715cca67dd929c2bd6403
Team-Automata <cole@teamautomata.com>
2016-12-28 21:33:25 -0700

    v2.0.1.a
    - Updates to homepage
    - Added Who we Are page
    - Added css for general page content
    - Added more demo images

--- js/main.js
+++ js/main.js
@@ -24,14 +24,35 @@ function getWindowSize() {
 
 	return [viewPortWidth, viewPortHeight];
 }
-	
-$(window).scroll(function () {
+
+function setHeaderFooter(){
 	[viewPortWidth, viewPortHeight] = getWindowSize();
 
-	$("#navbar").css("top", Math.min(0, (4*viewPortHeight) - $(this).scrollTop()));
-	$("#mainMenu").css("top", Math.min(0, (4*viewPortHeight) - $(this).scrollTop()));
-	$("#floatingFooter").css("bottom", Math.max(0, $(this).scrollTop() - (4*viewPortHeight)));
+	var v = viewPortHeight;
+	var h = document.getElementById('bodyContent').clientHeight;
+	h -= v;
+	h = Math.min(0, abs(h));
+
+	$("#navbar").css("top", Math.min(0, (h) - $(this).scrollTop()));
+	$("#mainMenu").css("top", Math.min(0, (h) - $(this).scrollTop()));
+	$(".floatingFooter").css("bottom", Math.max(0, $(this).scrollTop() - (h)));
+}
+
+$(window).scroll(function () {
+	setHeaderFooter();
 });
+
+// Detect Navbar Height
+function setNavbarSpacer() {
+	var h = document.getElementById('navbar').clientHeight;
+	h += "px";
+
+	var elements = document.getElementsByClassName('navbarSpacer');
+
+	for(i = 0; i < elements.length; i++) {
+		elements[i].style.height = h;
+	}
+}
 
 // Scroll to specific section
 function scroll(multiplier) {
@@ -47,10 +68,6 @@ function scroll(multiplier) {
 
 // Toggle Main Menu
 function openMenu() {
-	var h = document.getElementById('navbar').clientHeight;
-	h += "px";
-	
-	document.getElementById('navbarBack').style.height = h;
 	$("#navbarBack").slideDown(500);
 	$("#mainMenu").slideDown(500);
 	$("#menuOpen").hide();
@@ -72,42 +89,38 @@ $(function () {
 		if (window.location.hash && window.location.hash != "#home") {
 			// Hash found
 			var hash = window.location.hash;
-			hash += 'Body';
+			hash += 'page';
+			var hashFooter = hash;
+			hashFooter += 'Footer';
 			//alert(hash);
 			closeMenu();
-			$("#floatingFooter").slideUp(750);
-			$("#homepage").slideUp(500);
+			$(".floatingFooter").slideUp(500);
+			$(".page").fadeOut(500);
 
-			/*
-			$("#banner").slideUp();
-			$("#bannerSpacer").slideUp();
-			$("#logo").animate({ margin: '20px 0 0 0' });
-			$(".footer").fadeOut(400).delay(400);
-			$(".bodycontent").fadeOut(400).delay(400);
-			$(hash).fadeIn(400).delay(400);
-			$("#footerPage").fadeIn(400);
-			*/
+			$("#fixedFooter").fadeOut(500).delay(50).fadeIn(500);
+
+			$(hash).delay(550).fadeIn(500);
+			$(hashFooter).delay(550).slideDown(500);
+
+			//$("#logo").animate({ margin: '20px 0 0 0' });
 		}
 		else {
-			// No hash found
+			// No hash found or #home
 			closeMenu();
-			scroll(0);
-			$("#floatingFooter").slideDown(750);
-			$("#homepage").slideDown(2000);
+			$(".floatingFooter").not("#homepageFooter").slideUp(500);
+			$(".page").not("#homepage").fadeOut(500);
 
+			$("#fixedFooter").fadeOut(500).delay(50).fadeIn(500);
 
-			/*
-			$(".footer").fadeOut(400).delay(400);
-			$(".bodycontent").fadeOut(400).delay(400);
-			$("#banner").slideDown();
-			$("#bannerSpacer").slideDown();
-			$("#logo").animate({ margin: '20px 0 20px 0' });
-			$("#footerHome").fadeIn(400);
-			*/
+			$("#homepage").delay(550).fadeIn(500);
+			$("#homepageFooter").delay(550).slideDown(500);
 		}
 	});
 });
 
 $(document).ready(function() {
+	$(window).hashchange();
+	setNavbarSpacer();
+	setHeaderFooter();
 	$('.popup-youtube').magnificPopup({type:'iframe'});
 });
